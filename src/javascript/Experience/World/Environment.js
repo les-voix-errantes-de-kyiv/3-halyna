7b53a5701d87ec3f084a9e9f4ec9b24b4ce510c5
Nehemia Telusma <telusma.nehemia@gmail.com>
2024-01-24 15:42:42 +0100

    feat: fog added + v1 light

--- src/javascript/Experience/World/Environment.js
+++ src/javascript/Experience/World/Environment.js
@@ -9,17 +9,25 @@ export default class Environment
         this.scene = this.experience.scene
         this.ressources = this.experience.resources
 
-        // this.setFog()
+        this.setFog()
+        // this.setDirectionalLight()
         this.setAmbiantLight()
     }
 
     setFog(){
-      this.fog = new THREE.Fog('#cccccc', 0, 6)
+      this.fog = new THREE.Fog('#354569', 7, 60)
       this.scene.fog = this.fog
     }
 
     setAmbiantLight(){
-      this.ambientLight = new THREE.AmbientLight('#ffffff', 1)
+      this.ambientLight = new THREE.AmbientLight('#D2E1EE', 0.5)
       this.scene.add(this.ambientLight)
+    }
+
+    setDirectionalLight(){
+      this.directionalLight = new THREE.DirectionalLight('#D2E1EE', 1)
+      this.scene.add(this.directionalLight, this.directionalLight.target)
+      this.directionalLightHelper = new THREE.DirectionalLightHelper( this.directionalLight );
+      this.scene.add( this.directionalLightHelper );
     }
 }
