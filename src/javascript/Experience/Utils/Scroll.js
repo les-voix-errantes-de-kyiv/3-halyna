217ae09224280a648998958ad7aa471d3445c39f
Nehemia Telusma <telusma.nehemia@gmail.com>
2024-01-24 01:03:34 +0100

    feat: using scrolltrigger to anim section in slide

--- src/javascript/Experience/Utils/Scroll.js
+++ src/javascript/Experience/Utils/Scroll.js
@@ -1,22 +1,56 @@
-import Animation from '../Animation'
-import EventEmitter from './EventEmitter'
+import Experience from '../Experience'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-export default class Scroll extends EventEmitter{
-  constructor(){
-    super()
+gsap.registerPlugin(ScrollTrigger);
+
+export default class Scroll {
+  constructor() {
+    this.experience = new Experience();
+    this.resources = this.experience.resources
+    this.camera = this.experience.camera.instance
 
     // Setup
-    this.animation = new Animation()
-    this.width = window.innerWidth
-    this.height = window.innerHeight
-    this.pixelRatio = Math.min(window.devicePixelRatio, 2)
-    
-    // // Scroll event
-    // this.scrollY = window.scrollY;
-    // window.addEventListener('scroll', ()=>{
-    //   // console.log(this.scrollY);
-    //   this.scrollY = window.scrollY;
-    //   this.animation.animateCamera();
-    // })
+    this.resources.on('ready', () => {
+      this.setupScrollAnimations();
+    })
+  }
+
+  setupScrollAnimations() {
+    const slides = document.querySelectorAll('.slide');
+
+    slides.forEach((slide, index) => {
+      const sections = slide.querySelectorAll('section');
+
+      sections.forEach((section, sectionIndex) => {
+        const animationType = section.dataset.animation;
+        const slideId = slide.getAttribute('data-slide');
+
+        if (animationType === 'translate') {
+          const slideElt = this.experience.world[`slide${slideId}`].mesh.position
+          console.log(slideId);
+          gsap.to(slideElt, {
+            x: 11,
+            scrollTrigger: {
+              trigger: section,
+              start: 'top center',
+              end: 'bottom center',
+              scrub: true,
+              // markers: true
+            },
+          });
+        } else if (animationType === 'cameraMove') {
+          gsap.to(this.camera.position, {
+            z: 12,
+            scrollTrigger: {
+              trigger: section,
+              start: 'top center',
+              end: 'bottom center',
+              scrub: true,
+            },
+          });
+        }
+      });
+    });
   }
 }
